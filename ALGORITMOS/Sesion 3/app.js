9e4c754672e6a1784c0aac7218edd4ae7e829d99
joseberlanga <jjcberlanga_91@hotmail.com>
2021-11-23 21:07:24 -0600

    initial docs

---
+++ ALGORITMOS/Sesion 3/app.js
@@ -0,0 +1,71 @@
+let age = parseInt(prompt("Introduce la edad"))
+let weeks = parseInt(prompt("Introduce las semanas cotizadas"))
+    if (age > 60 & weeks > 750){
+        console.log("Ya se puede jubuilar")
+    } else{
+        console.log(":( Aun no te puedes jubilar");
+
+    }
+
+
+
+let age = parseInt(prompt("Introduce la edad"))
+
+if(weather = "lluvioso" & weather = "soleado"){
+    console.log();
+}
+
+    
+
+
+
+
+
+
+
+
+if (age > 60){
+    console.log("Se puede pensionar")
+}
+
+let weeks = 750
+
+if (weeks > 750){
+    console.log("Se puede pensionar")
+}
+
+console.log("Si se puede pensionar")
+
+let rain = true
+
+if(rain === true){
+    console.log("ver peliculas")
+} else{
+    console.log("sacar a pasear al perro")
+}
+
+console.log("Ver peliculas o pasear al perro")
+
+let operation = prompt("Operacion que deseas realizar") // suma, resta, division, multiplicacion
+let number1 = parseInt(prompt("Introduce la edad"))
+let number2 = parseInt(prompt("introduce las semanas cotizadas"))
+console.log("operation:", operation) 
+
+switch (operation) { 
+    case "suma":
+        console.log("la suma es:", number1 + number2)
+    break
+    case "resta":
+        console.log("la resta es:", number1 - number2)
+    break
+    case "division":
+        console.log("la division es:", number1 / number2)
+    break
+    case "multiplicacion":
+        console.log("la multiplicacion es:", number1 * number2)
+    break
+    default:
+        console.log("operacion permitida")
+        break
+}
+
