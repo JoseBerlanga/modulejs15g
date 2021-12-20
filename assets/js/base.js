751e1c071375aeb5bc501685db02d82fab557d60
joseberlanga <jjcberlanga_91@hotmail.com>
2021-12-19 21:20:37 -0600

    clase dos

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,43 +1,38 @@
-/*
-	Ejercicio 1:
-	Pedir 2 variables por prompt
-	Si a > b dividir a entre b
-	Si a < b sumar a más b
-	si a == b multiplicar ambos numeros
-	Imprimir el resultado en consola
-*/
+// celsius to fahrenheit
+const celsiusGrade = parseInt(prompt('Grados celsius en tu localidad'))
+if( !isNaN(celsiusGrade) ){
+
+	let fahrenheitGrade  = (celsiusGrade * 9/5) + 32
+	console.log(`La temperatura actual es de: ${fahrenheitGrade}ºF`)
+	 
+} else {
+	console.log('Es necesario un número')
+}
 
 
-const a = parseInt(prompt('Inroduce valor A'))
-const b = parseInt(prompt('Inroduce valor B'))
-// if(isNaN(a) === false && isNaN(b) === false){
-//     if (a > b){
-//         console.log(a / b)
-//     } else if (a < b){
-//         console.log(a + b)
-//     } else if (a == b){
-//         console.log(a * b)
+// Ejercicio 2
+
+// const weather = prompt('como esta el clima en su ciudad? \n 1. Soleado \n 2. Lluvioso \n 2. Nevando \n 2. Nublado ')
+// const celsiusGrade = parseInt(prompt('¿Cual es la temperatura en tu localidad?'))
+// const fahrenheitGrade  = (celsiusGrade * 9/5) + 32
+
+// if(!isNaN(celsiusGrade && typeof weather === 'string') ){
+//     switch(weather) {
+//         case 'Soleado':
+//             console.log(`En tu ciudad, el día está 'Soleado' con una temperatura de ${fahrenheitGrade}ºF`)
+//         break
+//         case 'Lluvioso':
+//             console.log(`En tu ciudad, el día está Lluvioso con una temperatura de ${fahrenheitGrade}ºF`)
+//         break
+//         case 'Nevando':
+//             console.log(`En tu ciudad, el día está Nevando con una temperatura de ${fahrenheitGrade}ºF`)
+//         break
+//         case 'Nublado':
+//             console.log(`En tu ciudad, el día está Nublado con una temperatura de ${fahrenheitGrade}ºF`)
+//         break
+//         default:
+//             console.log(`No entiendo cual es el clima`)
 //     }
-// }else {
-//     console.log('Introduce solo numeros');
-// }
-
-!isNaN(a) && !isNaN(b) ?
-    a > b ? console.log(a /b)
-    : a < b ? console.log(a + b)
-    : a === b ? console.log(a * b)
-    : ''
-:
-console.error('Introduce solo numeros');
-
-
-//    console.log('tabla del ' + (i+ 3) + '.') 
-//    console.log(`tabla del ${i+3}`)
-// for(i = 1; i <= 10; i++ ){
-
-//    for(j = 1; j <= 10 ; j++) {
-//     console.log(`${i} x ${j} = ${ i * j }`)
-//    } 
-
+//     console.log(`En tu ciudad, el día está ${weather} con una temperatura de ${fahrenheitGrade}ºF`)
 // }
 
